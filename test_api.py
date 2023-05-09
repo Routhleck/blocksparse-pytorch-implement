eac2505448fd53bf15c357a98b225bf6e63ca4f0
Routhleck <1310722434@qq.com>
2023-05-09 11:07:09 +0800

    更新CPU版本的blocksparse matmul

--- test_api.py
+++ test_api.py
@@ -1,17 +1,31 @@
 from blocksparse.block_sparse_api import blocksparse_matmul
 import torch
-m = 32
+m = 64
 k = 32
-n = 32
-device = 'cuda'
-A = torch.randn((m,) + (k,), device=device)
-B = torch.randn((k,) + (n,), device=device) # * (bm.random.rand(k, n) < 0.1)
-A_temp = A
-B_temp = B
-C1 = blocksparse_matmul(A, B)
-C_true = torch.matmul(A, B)
-print(C1.cpu().numpy())
-print(C_true.cpu().numpy())
-print((C1.cpu().numpy() - C_true.cpu().numpy()))
-# print(C3.numpy())
-# print((C1 - C3).numpy())
+n = 64
+def gpu_test(device = 'cuda'):
+    A = torch.randn((m,) + (k,), device=device)
+    B = torch.randn((k,) + (n,), device=device) # * (bm.random.rand(k, n) < 0.1)
+    A_temp = A
+    B_temp = B
+    C1 = blocksparse_matmul(A, B, device=device)
+    C_true = torch.matmul(A, B)
+    print(C1.cpu().numpy())
+    print(C_true.cpu().numpy())
+    print((C1.cpu().numpy() - C_true.cpu().numpy()))
+    # print(C3.numpy())
+    # print((C1 - C3).numpy())
+
+def cpu_test(device = 'cpu'):
+    A = torch.randn((m,) + (k,), device=device)
+    B = torch.randn((k,) + (n,), device=device)
+    A_temp = A
+    B_temp = B
+    C1 = blocksparse_matmul(A, B)
+    C_true = torch.matmul(A, B)
+    print(C1.cpu().numpy())
+    print(C_true.cpu().numpy())
+    print((C1.cpu().numpy() - C_true.cpu().numpy()))
+
+gpu_test()
+cpu_test()
